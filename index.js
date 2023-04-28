2f0251528963f7c2bc4b2730b786f169bfb3db6f
Paul <pmacellaro7@aol.com>
2023-04-28 01:24:02 -0400

    Cats, a lot of cats

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyAppendCat(cat) {
+    cats.push(cat);
+    return cats;}
+    
+    function destructivelyPrependCat(cat2) {
+      cats.unshift("Bob");
+      return cats;}
+function destructivelyRemoveLastCat(cat){
+  cats.pop();
+  return cats;
+}
+function destructivelyRemoveFirstCat(cat){
+  cats.shift();
+  return cats;
+}
+function appendCat(Broom){
+  const Allcats = [ ...cats, "Broom"]
+  return Allcats
+}
+function prependCat(Arnold){
+  const Allcats2 = ["Arnold", ...cats]
+  return Allcats2
+}
+function removeLastCat(cats3){
+   cats3 = cats.slice(0,2)
+  return cats3
+}  
+function removeFirstCat(cats4){
+  cats4 = cats.slice(1)
+  return cats4
+}
